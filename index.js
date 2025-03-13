3cc5b0bc9f28097d68ab11018cbbf731bcb4aaa1
LindsellR <lindsellrichard@gmail.com>
2025-03-14 10:44:58 +1100

    Added middleware function to remove trailing slash on url

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(morgan('common'));
 
 // Serve static files from the 'public' directory
-app.use('/documentation.html', express.static( 'public'));
+app.use('/documentation.html', express.static( 'public/documentation.html'));
 
 // Route for your JSON response
 app.get('/movies', (req, res) => {
@@ -16,6 +16,15 @@ app.get('/movies', (req, res) => {
 app.get('/', (req, res) => {
     res.send('Welcome to my Movie Geek app!');
 });
+
+app.use((req, res, next) => {
+  if (req.path(-1) === '/' && req.path.length > 1) {
+    res.redirect(301, req.path.slice(0, -1) + req.url.slice(req.path.length));
+  } else {
+    next();
+  }
+});
+
  //Error message
 app.use((err, req, res, next) => {
     console.error(err.stack);
